20199051ae465d0296575e76556d8948877204db
Mike Jones <mjones@medasync.com>
2020-08-12 20:08:41 -0700

    get gradlew test working

---
+++ src/test/java/com/michaelwilliamjones/hellojava/greeting/GreeterTest.java
@@ -0,0 +1,22 @@
+package com.michaelwilliamjones.hellojava.greeting;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+@Test
+public class GreeterTest {
+    private static final String TEST_GREETING = "this is a test greeting";
+
+    @Test
+    public void getGreeting_returnsHelloWorld() {
+        // arrange
+        Greeter greeter = new Greeter(TEST_GREETING);
+
+        // act
+        String greeting = greeter.getGreeting();
+
+        // assert
+        assertEquals(greeting, TEST_GREETING);
+    }
+}
